e21dc4ea4c30078eeb6686c3afb6d33107c3b277
Jake Litwicki <jake.litwicki@gmail.com>
2020-07-11 23:24:27 -0700

    update build script

---
+++ build.sh
@@ -0,0 +1,11 @@
+echo "Building Docker image"
+docker build --tag amazonlinux:nodejs .
+echo "Installing Sharp and other modules"
+docker run --rm --volume ${PWD}/lambda/origin-response-function:/build amazonlinux:nodejs /bin/bash -c "source ~/.bashrc; npm init -f -y; npm install sharp --save; npm install querystring --save; npm install --only=prod"
+echo "Building dist"
+mkdir -p dist
+echo "Build origin-response-function"
+cd lambda/origin-response-function && zip -FS -q -r ../../dist/origin-response-function.zip * && cd ../..
+echo "Build viewer-request-function"
+cd lambda/viewer-request-function && zip -FS -q -r ../../dist/viewer-request-function.zip * && cd ../..
+echo "Done building stuff"
--- deploy.sh
+++
@@ -1,4 +0,0 @@
-docker build --tag amazonlinux:nodejs .
-docker run --rm --volume ${PWD}/lambda/origin-response-function:/build amazonlinux:nodejs /bin/bash -c "source ~/.bashrc; npm init -f -y; npm install sharp --save; npm install querystring --save; npm install --only=prod"
-mkdir -p dist && cd lambda/origin-response-function && zip -FS -q -r ../../dist/origin-response-function.zip * && cd ../..
-mkdir -p dist && cd lambda/viewer-request-function && zip -FS -q -r ../../dist/viewer-request-function.zip * && cd ../..
